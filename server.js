44e6069e52850567f6368bf7dca0a4c19575ea43
siva1933 <b121933rgukt@gmail.com>
2022-12-26 09:47:47 +0530

    changes added

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 
 const express = require('express');
+const rsaPemToJwk = require('./rsa-pem-to-jwk');
 const app = express();
+const fs = require("fs");
+// openssl genrsa -out privateKey.pem 512
+var privateKey = fs.readFileSync("./privateKet.pem");
+// openssl rsa -in privateKey.pem -pubout -out publicKey.pem
+var publicKey = fs.readFileSync("./publicKey.pem");
 
-app.use('/', (req,res)=>{
+app.use('/', (req, res) => {
+  let private = rsaPemToJwk(privateKey.toString(), undefined, 'private')
+  let public = rsaPemToJwk(publicKey.toString(), undefined, 'public')
   res.json({
-    message:"Server Up and Running"
+    message: "Server Up and Running",
+    keys:[public,private]
   })
 });
 
